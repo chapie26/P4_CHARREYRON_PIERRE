fb1306a25b08eddd1b14584cb0e121524b89b577
Pierre Charreyron <pierre@iMac-de-Pierre.local>
2020-03-10 11:37:20 +0100

    affichage de l'accueil separer du traitement a la base de donnees et fonctionnel

---
+++ affichageAccueil.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Blog de Mr Jean Forteroche</title>
+        <link href="style.css" rel="stylesheet" />
+    </head>
+
+    <body>
+        <h1>Mon super blog!</h1>
+        <h2>Derniers billets du blog!</h2>
+
+        <?php
+        while ($donnees = $req->fetch())
+        {
+        ?>
+            <div>
+                <h3>
+                    <?php  echo htmlspecialchars($donnees['titre']); ?>
+                    <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+                </h3>
+                <p>
+                    <?php echo htmlspecialchars($donnees['contenu']); ?><br />
+                    <em><a href="commentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
+                </p>
+            </div>
+        <?php
+        }
+        $req->closeCursor();
+        ?>
+    </body>
+</html>
