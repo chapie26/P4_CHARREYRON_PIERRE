f8f3b557bbf6a0f1b0aaabc05bdf5a57c0554d91
Pierre Charreyron <pierre@iMac-de-Pierre.local>
2020-03-10 19:15:26 +0100

    Ajout du template

--- indexView.php
+++ indexView.php
@@ -1,32 +1,27 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Blog de Mr Jean Forteroche</title>
-        <link href="style.css" rel="stylesheet" />
-    </head>
+<?php $title = 'Blog de Mr Jean FORTEROCHE'; ?>
 
-    <body>
-        <h1>Mon super blog!</h1>
-        <h2>Derniers billets du blog :</h2>
+<?php ob_start(); ?>
+<h1>Mon super blog!</h1>
+<h2>Derniers billets du blog :</h2>
 
-        <?php
-        while ($data = $posts->fetch())
-        {
-        ?>
-            <div>
-                <h3>
-                    <?= htmlspecialchars($data['title']); ?>
-                    <em>le <?= $data['creation_date_fr']; ?></em>
-                </h3>
-                <p>
-                    <?= htmlspecialchars($data['content']); ?><br />
-                    <em><a href="post.php?id=<?= $data['id']; ?>">Commentaires</a></em>
-                </p>
-            </div>
-        <?php
-        }
-        $posts->closeCursor();
-        ?>
-    </body>
-</html>
+<?php
+while ($data = $posts->fetch())
+{
+?>
+    <div>
+        <h3>
+            <?= htmlspecialchars($data['title']); ?>
+            <em>le <?= $data['creation_date_fr']; ?></em>
+        </h3>
+        <p>
+            <?= htmlspecialchars($data['content']); ?><br />
+            <em><a href="post.php?id=<?= $data['id']; ?>">Commentaires</a></em>
+        </p>
+    </div>
+<?php
+}
+$posts->closeCursor();
+?>
+<?php $content = obj_get_clean(); ?>
+
+<?php require('template.php'); ?>
--- postView.php
+++ postView.php
@@ -1,35 +1,31 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Blog de Mr Jean Forteroche</title>
-        <link href="style.css" rel="stylesheet" />
-    </head>
+<?php $title = 'Blog de Mr Jean FORTEROCHE'; ?>
 
-    <body>
-        <h1>Mon super blog!</h1>
-        <p><a href="index.php">Retour à la liste des billets</a></p>
+<?php ob_start(); ?>
+<h1>Mon super blog!</h1>
+<p><a href="index.php">Retour à la liste des billets</a></p>
 
-        <div>
-            <h3>
-                <?php  echo htmlspecialchars($post['title']); ?>
-                <em>le <?php echo $post['creation_date_fr']; ?></em>
-            </h3>
-            <p>
-                <?php echo htmlspecialchars($post['content']); ?>
-            </p>
-        </div>
+<div>
+    <h3>
+        <?php  echo htmlspecialchars($post['title']); ?>
+        <em>le <?php echo $post['creation_date_fr']; ?></em>
+    </h3>
+    <p>
+        <?php echo htmlspecialchars($post['content']); ?>
+    </p>
+</div>
 
-        <h2>Commentaires</h2>
+<h2>Commentaires</h2>
 
-        <?php
-        While ($comment = $comments->fetch())
-        {
-        ?>
-            <p><strong><?php echo htmlspecialchars($comment['author']); ?></strong> le <?php echo $comment['comment_date_fr']; ?></p>
-            <p><?php echo htmlspecialchars($comment['comment']); ?></p>
-        <?php
-        }
-        ?>
-    </body>
-</html>
+<?php
+While ($comment = $comments->fetch())
+{
+?>
+    <p><strong><?php echo htmlspecialchars($comment['author']); ?></strong> le <?php echo $comment['comment_date_fr']; ?></p>
+    <p><?php echo htmlspecialchars($comment['comment']); ?></p>
+<?php
+}
+?>
+
+<?php obj_get_clean(); ?>
+
+<?php require('template.php'); ?>
