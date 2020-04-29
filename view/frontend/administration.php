e32cc16f1ed57f4b872f120917ee41454f952a30
Pierre Charreyron <pierre@iMac-de-Pierre.local>
2020-04-29 18:02:31 +0200

    passage tiny en fr, mise au propre de tiny(plus de plugin, balise form afin de recuperer...)

--- view/frontend/administration.php
+++ view/frontend/administration.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <script src="https://cdn.tiny.cloud/1/ixuygdvn8dxxs0e5sl10ihp8bk79mm50lr20kondefxlz84e/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-  <script>tinymce.init({ selector:'textarea' });</script>
-</head>
-<body>
-  <textarea>Next, get a free Tiny Cloud API key!</textarea>
-</body>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <script src="https://cdn.tiny.cloud/1/ixuygdvn8dxxs0e5sl10ihp8bk79mm50lr20kondefxlz84e/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+        <script>tinymce.init({ selector:'#mytextarea', language: 'fr_FR'});</script>
+    </head>
+
+    <body>
+        <p><a href="index.php">Retour à la liste des billets</a></p>
+        <form method="post" action="">
+            <textarea id="mytextarea">Hello, World!!</textarea>
+        </form>
+    </body>
 </html>
